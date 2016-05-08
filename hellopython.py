ed32a6efbfa9aaef4645f55dd5daeedb049267c8
ZZJMAC <hljzzj@qq.com>
2016-05-08 23:53:30 +0800

    2

--- hellopython.py
+++ hellopython.py
@@ -8,3 +8,9 @@ a = (1+2+3+4)*8
 b = a*a
 print a,b
 print b/a*2*32
+print ("I love fishc.com")  #前妻系歹
+#printf ("I love fishc.com");    #小三系列
+print (5+3)
+print (12345678987654321*98765432123456789)
+print ("well water" + " river")
+print ("I love fishc.com\n" *8)
